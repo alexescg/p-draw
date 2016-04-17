d9833dfa1ed65b9984606781bbb3a71ea06181d3
Alex Escobedo <aeg0204@gmail.com>
2016-04-17 13:48:54 -0600

    Se agrega migracion principal

---
+++ app.js
@@ -0,0 +1,95 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var ConnectRoles = require('connect-roles');
+var flash = require('connect-flash');
+var roles = new ConnectRoles({
+    failureHandler: function (req, res, action) {
+        // optional function to customise code that runs when
+        // user fails authorisation
+        var accept = req.headers.accept || '';
+        res.status(403);
+        if (~accept.indexOf('html')) {
+            res.render('access-denied', {action: action});
+        } else {
+            res.send('Access Denied - You don\'t have permission to: ' + action);
+        }
+    }
+});
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require("express-session");
+//
+// var configDB = require('./config/database.js');
+// mongoose.connect(configDB.url);
+//
+// // var Usuario = require('./models/usuarios').Usuario;
+//
+// //Configurations
+// //TODO: descomentar logger
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json()); //Application JSON
+app.use(bodyParser.urlencoded({extended: true}));// Multipart con array
+//
+// //View engine
+app.set("view engine", "jade");
+//
+//
+app.use("/static", express.static(__dirname + "/static"));
+//
+//
+app.use(session({
+    secret: "455e96f6c76b60d39f549f2f7a1830f1", //Es un hash que identifica  nuestra aplicacion de otras aplicaciones express
+    resave: false,//Cada vez que se aga un request se tiene que guardar o rehacer la sesión
+    saveUninitialized: false // Sirve para reinicializar la sesión cada vez que se hace un request
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(roles.middleware());
+app.use(flash());
+//
+roles.use(function (req, action) {
+    if (!req.isAuthenticated()) return action === '/login';
+});
+//
+// //Scrum Master puede ver todo
+// roles.use(function (req) {
+//     if (req.user.rolActual === 'scrum-master') {
+//         return true;
+//     }
+// });
+//
+// //Paginas de Product Owner
+// roles.use('product-owner page', function (req) {
+//     if (req.user.rolActual === 'proyect-manager') {
+//         return true;
+//     }
+// })
+//
+// roles.use('product-owner page', function (req) {
+//     if (req.user.rolActual === 'product-owner') {
+//         return true;
+//     }
+// })
+//
+// //Paginas de Developer
+// roles.use('developer page', function (req) {
+//     if (req.user.rolActual === 'developer' ) {
+//         return true;
+//     }
+// })
+//
+// require('./routes.js')(app, passport, roles);
+// require('./config/passport')(passport);
+
+app.get('/', function(req,res){
+    res.render('index');
+})
+
+app.listen(port, function () {
+    console.log("Escuchando desde el puerto " + port);
+});
---
+++ routes.js
@@ -0,0 +1,308 @@
+var Usuario = require('./models/usuarios');
+var Proyecto = require('./models/proyectos');
+var Rol = require('./models/roles')
+
+module.exports = function (app, passport, roles) {
+
+    app.get("/", function (req, res) {
+        res.render("login");
+    });
+
+    app.get('/login', function (req, res) {
+        res.render('login', {message: req.flash('loginMessage')});
+    });
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/home',
+        failureRedirect: '/login',
+        failureFlash: true
+    }));
+
+    app.get('/auth/facebook', passport.authenticate('facebook'));
+
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect: '/home',
+            failureRedirect: '/login'
+        }));
+
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect: '/home',
+            failureRedirect: '/login'
+        }));
+
+    app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+
+    app.get('/auth/google/callback',
+        passport.authenticate('google', {
+            successRedirect: '/home',
+            failureRedirect: '/login'
+        }));
+
+
+    app.get('/register', function (req, res) {
+        res.render('register', {message: req.flash('signupMessage')});
+    });
+
+    app.post('/register', passport.authenticate('local-signup', {
+        successRedirect: '/profile',
+        failureRedirect: '/register',
+        failureFlash: true
+    }));
+
+
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile',
+            {
+                message: req.flash('signupMessage'),
+                user: req.user
+            });
+    });
+
+
+    app.post('/profile', isLoggedIn, function (req, res) {
+        var usuario = new Usuario(req.user);
+        usuario.username = req.body.username;
+        usuario.nombre = req.body.nombre;
+        usuario.apellidos = req.body.apellidos;
+        usuario.fechaNacimiento = req.body.fechaNacimiento;
+        usuario.rfc = req.body.rfc;
+        usuario.curp = req.body.curp;
+        usuario.domicilio = req.body.domicilio;
+        usuario.rolActual = req.body.rolActual;
+
+        usuario.save(function (err, user) {
+                if (err) {
+                    console.log(err);
+                    res.render("profile", {
+                        message: req.flash('Error al guardar datos.'),
+                        user: req.user
+                    });
+                } else {
+                    res.render("profile", {
+                        message: req.flash('Exito!'),
+                        user: user
+                    });
+                }
+            }
+        );
+    });
+
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect('/login');
+    }
+
+    app.get("/main", isLoggedIn, function (req, response) {
+        response.render("templates/main");
+    });
+
+    app.get("/home", isLoggedIn, function (req, response) {
+        var protectos = [];
+        Proyecto.find({"participantes.usuario": req.user._id})
+            .exec(function (err, proyectos) {
+                if (proyectos != "") {
+                    Proyecto.populate(proyectos, {
+                        path: 'participantes.usuario',
+                        model: 'Usuario'
+                    }, function (err, proyectos) {
+                        response.render("dashboard", {usuario: req.user, proyectosTotal: proyectos});
+                    });
+                } else {
+                    response.render("proyectos/proyectosBlank", {usuario: req.user});
+                }
+            })
+    });
+
+    app.get("/detalleproyecto", isLoggedIn, function (req, response) {
+        if (req.query.proyectoElegido === undefined) {
+            req.query.proyectoElegido = req.session.proyecto;
+        }
+        Proyecto.findById({"_id": req.query.proyectoElegido}).populate({
+                path: 'participantes.usuario',
+                model: 'Usuario'
+            })
+            .exec(function (err, obj) {
+                req.session.proyecto = req.query.proyectoElegido
+                if (err) response.redirect("/home");
+                else
+                    var proyectManager;
+                var productOwner;
+                var scrumMaster;
+                var desarrolladores = [];
+                obj.participantes.forEach(function (participante) {
+                    switch (participante.rol) {
+                        case "product-owner":
+                            productOwner = participante.usuario;
+                            break;
+                        case "product-manager":
+                            proyectManager = participante.usuario;
+                            break;
+                        case "scrum-master":
+                            scrumMaster = participante.usuario;
+                            break;
+                        case "developer":
+                            desarrolladores.push(participante.usuario);
+                            break;
+                        default:
+
+                    }
+                });
+                console.log(desarrolladores);
+                response.render("./proyectos/detalleproyecto",
+                    {
+                        usuario: req.user,
+                        proyecto: obj,
+                        manager: proyectManager,
+                        scrumMaster: scrumMaster,
+                        owner: productOwner,
+                        desarrolladores: desarrolladores
+                    });
+            });
+    });
+    var sass;
+    app.get("/agregarScrum", isLoggedIn, function (req, response) {
+        sass = req.session;
+        Usuario.find({}).exec(function (err, usuarios) {
+            if (err) response.redirect("/")
+            else
+                response.render("./usuarios/agregarScrum", {usuarios: usuarios});
+        });
+    });
+
+    app.get("/agregarManager", isLoggedIn, function (req, response) {
+        sass = req.session;
+        Usuario.find({}).exec(function (err, usuarios) {
+            if (err) response.redirect("/")
+            else
+                response.render("./usuarios/agregarManager", {usuarios: usuarios});
+        });
+    });
+
+    app.get("/agregarDesarrollador", isLoggedIn, function (req, response) {
+        sass = req.session;
+        Usuario.find({}).exec(function (err, usuarios) {
+            if (err) response.redirect("/")
+            else
+                response.render("./usuarios/agregarDesarrolladores", {usuarios: usuarios});
+        });
+    });
+
+    app.post("/agregarScrum", isLoggedIn, function (req, response) {
+        req.session = sass;
+        var rol = new Rol({
+            rol: "scrum-master",
+            usuario: req.body.usuarioAsignado
+        });
+        Proyecto.update({_id: req.session.proyecto}, {$push: {participantes: {$each: [rol]}}}, {upsert: true}, function (err) {
+            if (err) {
+                response.redirect("/agregarScrum");
+            } else {
+                response.redirect("/detalleProyecto");
+            }
+        });
+
+    });
+
+    app.post("/agregarManager", isLoggedIn, function (req, response) {
+        req.session = sass;
+        var rol = new Rol({
+            rol: "product-manager",
+            usuario: req.body.usuarioAsignado
+        });
+        Proyecto.update({_id: req.session.proyecto}, {$push: {participantes: {$each: [rol]}}}, {upsert: true}, function (err) {
+            if (err) {
+                response.redirect("/agregarManager");
+            } else {
+                response.redirect("/detalleProyecto");
+            }
+        });
+    });
+
+    app.post("/agregarDesarrollador", isLoggedIn, function (req, response) {
+        req.session = sass;
+        var rol = new Rol({
+            rol: "developer",
+            usuario: req.body.usuarioAsignado
+        });
+        Proyecto.update({_id: req.session.proyecto}, {$push: {participantes: {$each: [rol]}}}, {upsert: true}, function (err) {
+            if (err) {
+                response.redirect("/agregarDesarrollador");
+            } else {
+                response.redirect("/detalleProyecto");
+            }
+        });
+    });
+
+    app.post("/crearUsuario", isLoggedIn, function (req, res) {
+        var usuario = new Usuario({
+            userName: req.body.userName,
+            email: req.body.email,
+            password: req.body.pass,
+            confirmarPassword: req.body.passConfirm,
+            nombre: req.body.nombre,
+            apellidos: req.body.apellidos,
+            fechaNacimiento: req.body.fechaDeNacimiento,
+            curp: req.body.CURP,
+            rfc: req.body.RFC,
+            domicilio: req.body.domicilio
+        });
+        //err tiene los errores que pueden pasar y obj el objeto a guardar.
+        usuario.save(function (err, obj) {
+            if (err) res.redirect("/crear", {obj: obj});
+        });
+        res.redirect("/");
+    });
+
+    app.get("/empleados", isLoggedIn, function (req, res) {
+        res.render("empleados");
+    });
+
+    app.post("/crearProyecto", function (req, res) {
+        var rol = new Rol({
+            rol: "product-owner",
+            usuario: req.user._id
+        });
+        var proyecto = new Proyecto({
+            nombreProyecto: req.body.nombreProyecto,
+            fechaSolicitud: Date(),
+            fechaArranque: req.body.fechaDeArranque,
+            descripcionProy: req.body.descripcion,
+        });
+        proyecto.participantes.push(rol);
+        //err tiene los errores que pueden pasar y obj el objeto a guardar.
+        proyecto.save(function (err, obj) {
+            if (err) res.redirect("/crear/proyecto", {obj: obj});
+            else res.redirect("/home");
+        });
+
+    });
+
+    app.get("/crear", isLoggedIn, function (req, res) {
+        res.render("usuarioBlank");
+    });
+
+    app.get("/cards", isLoggedIn, function (req, res) {
+        res.render("cosa");
+    });
+
+    app.get("/dashboard", isLoggedIn, function (req, res) {
+        res.render("index");
+    });
+
+    app.get("/detallesprint", isLoggedIn, function (req, res) {
+        res.render("detalleSprint");
+    });
+
+}
+;
